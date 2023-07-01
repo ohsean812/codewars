f94f78d57986874079e665cc310ffa990a67c527
Sean <ohsean812@gmail.com>
2023-06-30 23:06:21 -0400

    multiply kata complete

---
+++ 8kyu/multiply.js
@@ -0,0 +1,14 @@
+// This code does not execute properly. Try to figure out why.
+
+// function multiply(a, b){
+//     a * b
+//   }
+
+
+// My Solution
+
+// The function body doesn't have a return statement:
+
+function multiply(a, b){
+    return a * b
+}
