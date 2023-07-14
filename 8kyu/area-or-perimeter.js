7215d3a9510022ebeb3505a3ffbd8099883ef750
Sean <ohsean812@gmail.com>
2023-07-13 23:34:41 -0400

    area or perimeter solution refactored

--- 8kyu/area-or-perimeter.js
+++ 8kyu/area-or-perimeter.js
@@ -17,3 +17,8 @@ const areaOrPerimeter = function(l , w) {
       return ((l + w) * 2)
     }
 };
+
+// refactored
+const areaOrPerimeter = function(l , w) {
+  return (l === w) ? (l * w) : (l + w)*2
+};
