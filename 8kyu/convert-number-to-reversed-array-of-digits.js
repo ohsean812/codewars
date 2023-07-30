df8ba6d44211dbb93c5168fcfac7e9c7b1f2f177
Sean <ohsean812@gmail.com>
2023-07-30 11:26:45 -0400

    is n divisible by x and y kata completed

--- 8kyu/convert-number-to-reversed-array-of-digits.js
+++ 8kyu/convert-number-to-reversed-array-of-digits.js
@@ -12,3 +12,10 @@ function digitize(n) {
     let reversedArray = string.split("").reverse()
     return reversedArray.map(e => parseInt(e))
 }
+
+
+// Refactored
+
+function digitize(n) {
+    return String(n).split('').map(Number).reverse()
+}
