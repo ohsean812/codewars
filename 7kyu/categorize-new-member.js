8b8ab1241aa031079d32d64c610378f5c1395a3c
Sean <ohsean812@gmail.com>
2023-08-17 22:56:11 -0400

    categorize new member kata refactored

--- 7kyu/categorize-new-member.js
+++ 7kyu/categorize-new-member.js
@@ -26,3 +26,10 @@ function openOrSenior(data){
     }
     return result
 }
+
+
+// Refactored
+
+function openOrSenior(data){
+    return data.map(([age, handicap]) => (age >= 55 && handicap > 7) ? "Senior" : "Open")
+}
