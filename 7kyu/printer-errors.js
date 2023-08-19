ac665b6ef79a2572dbf70be7fc239ace82be54b4
Sean <ohsean812@gmail.com>
2023-08-18 22:11:13 -0400

    printer errors kata completed

---
+++ 7kyu/printer-errors.js
@@ -0,0 +1,18 @@
+// You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+
+// The string has a length greater or equal to one and contains only letters from ato z.
+
+// Examples:
+// s="aaabbbbhaijjjm"
+// printer_error(s) => "0/14"
+
+// s="aaaxbbbbyyhwawiwjjjwwm"
+// printer_error(s) => "8/22"
+
+
+// My Solution
+
+function printerError(s) {
+    let errors = s.split("").filter(letter => (letter > "m" && letter <= "z"))
+    return `${errors.length}/${s.length}`   
+}
