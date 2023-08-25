d569c29fa638453fc1a313fda716eac7c7565efb
Sean <ohsean812@gmail.com>
2023-08-24 23:02:32 -0400

    growth of a population kata recompleted

--- 7kyu/growth-of-a-population.js
+++ 7kyu/growth-of-a-population.js
@@ -37,3 +37,17 @@ function nbYear(p0, percent, aug, p) {
     
     return years
 }
+
+
+// My Solution 2 (to pass one last test)
+
+function nbYear(p0, percent, aug, p) {
+    let years = 0
+    
+    while (p0 < p){
+      p0 = Math.floor(p0 + p0 * percent/100 + aug)
+      years++
+    }
+    
+    return years
+}
