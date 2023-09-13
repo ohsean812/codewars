353ce686e769a692c3bc73be49e2bea6ed9b2dd3
Sean <ohsean812@gmail.com>
2023-09-12 23:59:45 -0400

    are you playing banjo kata refactored

--- 8kyu/are-you-playing-banjo.js
+++ 8kyu/are-you-playing-banjo.js
@@ -18,3 +18,10 @@ function areYouPlayingBanjo(name) {
       return `${name} does not play banjo`
     }
 }
+
+
+// Refactored
+
+function areYouPlayingBanjo(name) {
+    return name[0].toLowerCase() == "r" ? `${name} plays banjo` : `${name} does not play banjo`
+}
