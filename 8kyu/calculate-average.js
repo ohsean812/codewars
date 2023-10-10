7a9948670702e3d9e9d7734f5f4e291b1bcdb34e
Sean <ohsean812@gmail.com>
2023-10-09 23:44:51 -0400

    calculate average kata edited

--- 8kyu/calculate-average.js
+++ 8kyu/calculate-average.js
@@ -6,11 +6,12 @@
 // My Solution
 
 function findAverage(array) {
-    let sum = 0
-    for (let i = 0; i < array.length; i++){
-      sum += array[i]
-    } return sum / array.length
     if (array.length === 0){
       return 0;
+    } else {
+      let sum = 0
+      for (let i = 0; i < array.length; i++){
+        sum += array[i]
+      } return sum / array.length
     }
 }
